4d95c74f68dc45435d4a914e9b0f2436572790ab
Rowin <rowin.mol@youngcapital.nl>
2020-11-26 12:17:01 +0100

    docs: implemented dev documentation to storybook

---
+++ src/documentation/documentation.stories.js
@@ -0,0 +1,40 @@
+/* eslint-disable react/no-danger */
+import marked from 'marked';
+import React from 'react';
+import changeLog from '../../CHANGELOG.md';
+import packageJson from '../../package.json';
+import readMe from '../../README.md';
+import developerReadMe from '../../scripts/build/README_PACKAGE.md';
+
+import './documentation.css';
+
+export default {
+  title: 'Docunentation/Documentation',
+  parameters: {
+    options: {
+      showPanel: false,
+    },
+  },
+};
+
+function versionizeReadMe(readMeContent) {
+  const readMeLines = readMeContent.split('\n');
+  return [`${readMeLines[0]} (v${packageJson.version})`, ...readMeLines.slice(1)].join('\n');
+}
+
+export function LibraryDeveloper() {
+  return <div className='documentation-light' dangerouslySetInnerHTML={{ __html: marked(versionizeReadMe(readMe)) }} />;
+}
+
+export function PackageDeveloper() {
+  return (
+    <div
+      className='documentation-light'
+      dangerouslySetInnerHTML={{ __html: marked(versionizeReadMe(developerReadMe)) }}
+    />
+  );
+}
+
+export function ChangeLog() {
+  return <div className='documentation-light' dangerouslySetInnerHTML={{ __html: marked(changeLog) }} />;
+}
