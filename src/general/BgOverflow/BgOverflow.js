0caa60264ab81766f32e0f18901185067889e513
Rowin <rowin.mol@youngcapital.nl>
2020-12-24 13:24:09 +0100

    fix: reverse the bgoverflow type prop

--- src/general/BgOverflow/BgOverflow.js
+++ src/general/BgOverflow/BgOverflow.js
@@ -5,10 +5,10 @@ import PropTypes from 'prop-types';
 
 export const WorkFlowWrapper = styled.div`
   ${props =>
-    props.type === 'to_brand' &&
+    props.type === 'from_brand' &&
     'background: linear-gradient(0, #ffffff 11.74%, #2670d0)'}
   ${props =>
-    props.type === 'from_brand' &&
+    props.type === 'to_brand' &&
     'background: linear-gradient(180deg, #ffffff 11.74%, #2670d0)'}
 `;
 
@@ -21,7 +21,7 @@ const BgOverflow = ({ children, type = '', ...rest }) => {
 };
 
 BgOverflow.propTypes = {
-  type: PropTypes.oneOf(['to_brand', 'from_brand', '']),
+  type: PropTypes.oneOf(['to_brand', 'from_brand', 'none']),
   children: PropTypes.node,
 };
 
