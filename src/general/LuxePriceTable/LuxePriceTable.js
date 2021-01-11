f84005c3c0925d957921a089b0b2c55ed4360b1d
Rowin <rowin.mol@youngcapital.nl>
2021-01-11 22:05:07 +0100

    refactor: luxepricetable - extra padding instead of margin

--- src/general/LuxePriceTable/LuxePriceTable.js
+++ src/general/LuxePriceTable/LuxePriceTable.js
@@ -19,36 +19,38 @@ const LuxePriceTable = ({
 }) => {
   const multiple = content.length > 1;
   return (
-    <Row className={cn('z-0 my-20 text-gray-100', className)} {...rest}>
-      {content.map((table, index) => {
-        const isMiddle = multiple && index === 1;
-        return (
-          <Col
-            xs={12}
-            lg={multiple ? 4 : 12}
-            key={index}
-            className={cn(
-              'my-5 lg:my-0',
-              isMiddle
-                ? 'order-first lg:order-none lg:-mt-20 lg:-ml-2 lg:-mr-2 z-10'
-                : 'lg:-mt-10',
-            )}
-          >
-            <TableColumn
-              htmlSerializer={htmlSerializer}
-              linkResolver={linkResolver}
-              iconGenerator={iconGenerator}
-              showPrice={showPrice}
-              showButtons={showButtons}
-              multiple={multiple}
-              showInfo={showInfo}
-              table={table}
-              isMiddle={isMiddle}
-            />
-          </Col>
-        );
-      })}
-    </Row>
+    <div className={cn('py-20', className)} {...rest}>
+      <Row className={cn('z-0 mt-20 mb-10 text-gray-100')}>
+        {content.map((table, index) => {
+          const isMiddle = multiple && index === 1;
+          return (
+            <Col
+              xs={12}
+              lg={multiple ? 4 : 12}
+              key={index}
+              className={cn(
+                'my-5 lg:my-0',
+                isMiddle
+                  ? 'order-first lg:order-none lg:-mt-20 lg:-ml-2 lg:-mr-2 z-10'
+                  : 'lg:-mt-10',
+              )}
+            >
+              <TableColumn
+                htmlSerializer={htmlSerializer}
+                linkResolver={linkResolver}
+                iconGenerator={iconGenerator}
+                showPrice={showPrice}
+                showButtons={showButtons}
+                multiple={multiple}
+                showInfo={showInfo}
+                table={table}
+                isMiddle={isMiddle}
+              />
+            </Col>
+          );
+        })}
+      </Row>
+    </div>
   );
 };
 
